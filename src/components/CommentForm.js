339076bf9df82855101188b4f29d06d21f0d1044
Malli <mallikarjun.1rn17is104@gmail.com>
2020-06-16 14:46:28 +0530

    Assignment 3

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,11 +1,10 @@
 import  React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Label, Col, Row, Modal, ModalHeader, ModalBody } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Button, Label, Col, Row, Modal, ModalHeader, ModalBody} from 'reactstrap';
 import { Control, LocalForm, Errors} from 'react-redux-form';
+
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
-
 
 class CommentForm extends Component {
     
@@ -96,70 +95,4 @@ class CommentForm extends Component {
     }
 }
 
-
-function RenderDish({dish}){
-        return (
-            <div className="col-md-5 m-1">
-                            <Card>
-                                <CardImg width="100%" src = {dish.image} alt ={dish.name} />
-                                <CardBody>
-                                    <CardTitle>{dish.name}</CardTitle>
-                                    <CardText>{ dish.description }</CardText>
-                                </CardBody>
-                            </Card>
-            </div>
-        );
-    } 
-
-function RenderComments({comms}){
-        const com = comms.map((dish) => {
-            if(dish.comment != null ){
-                return (
-                    <div>
-                        <ul className = "list-unstyled">
-                            <li> {dish.comment} </li>
-                            <li> --{dish.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(dish.date)))}</li>
-                        </ul>
-                    </div>
-                );
-            }
-            else{
-                return ( <div></div> );
-            }
-        });
-        com.push(<CommentForm />);
-        return com;
-}
-
-const Dishdetail = (props) => {    
-        if(props.dish != null){
-                return(
-                    <div className = "container">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to ='/menu'>Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className  = "col-12">
-                            <h3> {props.dish.name} </h3>
-                            <hr />
-                        </div>
-                        
-                        <div className = "row">   
-                                <RenderDish dish = {props.dish} />
-                                <div className = "col-md-5 m-1">
-                                    <h4>Comments</h4>
-                                    <RenderComments comms = {props.comments} />
-                                </div>
-                        </div>
-                    </div>
-                );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
-}
-
-
-export default Dishdetail;
+export default CommentForm;

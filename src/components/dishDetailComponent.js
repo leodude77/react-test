762b3cb919ed911f554abfded38d6e163ede41eb
Malli <mallikarjun.1rn17is104@gmail.com>
2020-06-17 13:26:09 +0530

    redux thunk

--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -2,6 +2,7 @@ import  React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Label, Col, Row, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent'
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -131,6 +132,26 @@ function RenderComments({comms, addComment, dishId}){
 }
 
 const Dishdetail = (props) => {    
+        if(props.isLoading){
+            return (
+                <div className = "container">
+                    <div className = "row">
+                        <Loading />
+                    </div>    
+                </div>
+
+            );
+        }
+        else if (props.errMess){
+            return (
+                <div className = "container">
+                    <div className = "row">
+                        <h4>{props.errMess}</h4>
+                    </div>    
+                </div>
+
+            );
+        }
         if(props.dish != null){
                 return(
                     <div className = "container">
--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -1,1 +1,4 @@
 export const ADD_COMMENT = 'ADD_COMMENT';
+export const DISHES_LOADING = 'DISH_LOADING';
+export const DISHES_FAILED = 'DISH_FAILED';
+export const ADD_DISHES = 'ADD_DISHES';
